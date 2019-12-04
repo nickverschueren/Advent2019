7a79501c14ac659aa8b22bf77ce6577cf30c6537
NickVerschueren <nick.verschueren@euri.com>
2019-12-04 18:41:38 +0100

    Advent 4/25

---
+++ Advent4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startCode = "125730";
+            var endcode = "579381";
+            var currentCode = startCode;
+            var digits = currentCode.Select(c => int.Parse(c.ToString())).ToArray();
+            var goodCodes = new List<string>();
+            var numberOfDigits = digits.Length;
+
+            for (int i = 1; i < numberOfDigits; i++)
+            {
+                if (digits[i] < digits[i - 1])
+                    digits[i] = digits[i - 1];
+            }
+            currentCode = string.Join(null, digits);
+
+            do
+            {
+                /* For 1st star
+                if (digits.Distinct().Count() < numberOfDigits)
+                    goodCodes.Add(currentCode);
+                */
+
+                // For 2nd star
+                if (digits.GroupBy(d => d).Select(g => g.Count()).Contains(2))
+                    goodCodes.Add(currentCode);
+
+                for (int j = numberOfDigits - 1; j >= 0; j--)
+                {
+                    digits[j]++;
+                    if (digits[j] == 10) continue;
+                    for (int k = j; k < numberOfDigits; k++)
+                    {
+                        digits[k] = digits[j];
+                    }
+                    break;
+                }
+
+                currentCode = string.Join(null, digits);
+            } while (StringComparer.InvariantCulture.Compare(currentCode, endcode) < 0);
+
+            Console.WriteLine("Codes: " + string.Join(",", goodCodes));
+            Console.WriteLine("Count: " + goodCodes.Count);
+        }
+    }
+}
